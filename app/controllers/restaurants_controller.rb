f9e1065124d56f3a87cedcb801ad518e3ec51876
CarolinaCapilla <carolina.capilla@gmail.com>
2020-07-18 15:37:39 +0100

    finished both controllers and created correspondent views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,29 @@
 class RestaurantsController < ApplicationController
-  
+  before_action :set_restaurant, only: %i[show edit update destroy]
   def index
     @restaurants = Restaurant.all
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def edit
   end
 
   def show
-    
   end
 
-  def update  
+  def update
   end
 
   def destroy
@@ -26,7 +32,10 @@ class RestaurantsController < ApplicationController
   private
 
   def set_restaurant
-    
+    @restaurant = Restaurant.find(params[:id])
   end
-  
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,17 @@ Restaurant.destroy_all
 
 puts 'Creating restaurants...'
 
-10.times do
-  Restaurant.create(
-    name: Faker::Restaurant.name,
-    address:,
-    category:,
-    phone_number:,
-  )
+first_restaurant = { name: Faker::Restaurant.name, address: Faker::Address.street_address,
+                     category: %w[chinese italian japanese french belgian].sample,
+                     phone_number: Faker::PhoneNumber.cell_phone_in_e164 }
+
+second_restaurant = { name: Faker::Restaurant.name, address: Faker::Address.street_address,
+                      category: %w[chinese italian japanese french belgian].sample,
+                      phone_number: Faker::PhoneNumber.cell_phone_in_e164 }
+
+[first_restaurant, second_restaurant].each do |restaurant|
+  restaurant = Restaurant.create!(restaurant)
+  puts "Created #{restaurant.name}"
 end
+
+puts 'Finished'
